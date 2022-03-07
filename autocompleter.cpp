37a340e4c22eac3cc5b07d71a99411cd9000439b
RiceDaddy321 <0carlitos2@gmail.com>
2022-03-07 10:29:27 -0600

    made autocomplter.cpp and added all the functions that need to be implemented.

---
+++ autocompleter.cpp
@@ -0,0 +1,48 @@
+#include "autocompleter.h"
+
+Autocompleter::Autocompleter()
+{
+
+}
+
+void Autocompleter::insert(string x, int freq)
+{
+
+}
+
+int Autocompleter::size()
+{
+}
+
+void Autocompleter::completions(string x, vector<string> &T)
+{
+
+}
+int size_recurse(Node* p)
+{
+    
+}
+
+void completions_recurse(string x, Node* p, vector<Entry> &C)
+{
+
+}
+
+void insert_recurse(Entry e, Node* &p)
+{
+    
+}
+
+void rebalance(Node* &p)
+{
+
+}
+
+void right_rotate(Node* &p)
+{
+
+}
+void left_rotate(Node* &p)
+{
+
+}
