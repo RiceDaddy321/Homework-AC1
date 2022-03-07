d8b3a9bca4a49ff6c38b9003b03e3188575a59fb
RiceDaddy321 <0carlitos2@gmail.com>
2022-03-07 13:58:45 -0600

    Completed rotations (single and double)

--- autocompleter.cpp
+++ autocompleter.cpp
@@ -47,11 +47,48 @@ void Autocompleter::completions_recurse(string x, Node* p, vector<Entry> &C)
 
 void Autocompleter::insert_recurse(Entry e, Node* &p)
 {
-    
+    if(p == nullptr) // only worry about inserting
+    {
+        p = new Node;
+        p->e = e;
+    }
+    else
+    {
+        //pick a side
+        if(e.s < p->e.s)
+            insert_recurse(e, p->left);
+        else
+            insert_recurse(e, p->right);
+
+        //check if we screwed anything
+        update_height(p);
+        rebalance(p);
+    }
 }
 
 void Autocompleter::rebalance(Node* &p)
 {
+    //check for Right rotations
+    //happens when the left is bigger than right
+    if((p->left->height - p->right->height) > 1)
+    {
+        //check for left-right rotation
+        if((p->left->right->height - p->left->left->height) > 1)
+            left_rotate(p->left);
+
+        right_rotate(p);
+            
+    }
+    //check for left rotation
+    //happens when right > left
+    else if((p->right->height - p->left->height) > 1)
+    {
+        //check for right-left rotation
+        if((p->right->left->height - p->right->right->height) > 1)
+            right_rotate(p->right);
+
+        left_rotate(p);
+    }
 
 }
 
@@ -59,15 +96,31 @@ void Autocompleter::right_rotate(Node* &p)
 {
     //setup the variables
     Node* a = p;
-    Node* b = a->right;
-    Node* bl = b->left;
+    Node* b = a->left;
+    Node* br = b->right;
 
     //do the switcharoo
-    b->left = a;
-    a->right = bl;
     p = b;
+    b->right = a;
+    a->left = br;
+
+    //fix the heights
+    update_height(a);
+    update_height(b);    
 }
 void Autocompleter::left_rotate(Node* &p)
 {
+    //setup the variables
+    Node* a = p;
+    Node* b = p->right;
+    Node* bl = b->left;
 
+    //do the switch
+    p = b;
+    b->left = a;
+    a->right = bl;
+
+    //fix the heights
+    update_height(a);
+    update_height(b);
 }
